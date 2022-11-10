ed539f458803392abad59d502c5f27351b1da30e
August McAllister <august.chm@gmail.com>
2022-11-10 13:39:58 -0600

    Client is GETting data from db

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -31,6 +31,19 @@ function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
   
+  $.ajax({
+    method: 'GET',
+    url: '/koalas'
+  })
+  .then(function (response) {
+    console.log('AJAX GET successful');
+    console.log(response.rows);
+    // renderTable(response);
+  })
+  .catch(function (error) {
+    console.log('error', error);
+  })
+
 } // end getKoalas
 
 function saveKoala( newKoala ){
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,11 +1,40 @@
+const { Router } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool({
+  database: 'koalas', // name of database
+  host: 'localhost', // database server
+  port: 5432, // Postgres default
+  max: 10, // max queries at once
+  idleTimeoutMillis: 30000 // 30 seconds to try to connect before cancelling query
+});
 
+// not required, but useful for debugging:
+pool.on('connect', () => {
+  console.log('postgresql is connected!');
+});
+
+pool.on('error', (error) => {
+  console.log('error in postgres pool.', error);
+})
 
 // GET
+koalaRouter.get('/', (req, res) => {
+  let queryText = `SELECT * FROM koalas`;
 
+  pool.query(queryText)
+    .then( (response) => {
+      console.log('query GET successful');
+      res.send(response);
+    })
+    .catch( (error) => {
+      console.log('error in query GET:', error);
+    })
+})
 
 // POST
 
